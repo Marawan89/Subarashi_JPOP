036dde7ee2a6b621df5381e063a977dbf3f2af51
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-11-11 18:32:12 +0100

    recyclerView is kinda working but it gets on the 4th anime and the app crashes

--- app/src/main/java/studio/demo/subarashi_jpop/activities/AnimeListActivity.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/activities/AnimeListActivity.kt
@@ -1,24 +1,16 @@
 package studio.demo.subarashi_jpop.activities
 
-import AnimeListAdapter
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import studio.demo.subarashi_jpop.R
 import studio.demo.subarashi_jpop.adapters.AnimeListAdapter
 import studio.demo.subarashi_jpop.remote.RemoteApi
-import studio.demo.subarashi_jpop.remote.model.AnimeModel
 import studio.demo.subarashi_jpop.repositories.AnimeRepository
 import studio.demo.subarashi_jpop.viewmodel.AnimeListViewModel
 import studio.demo.subarashi_jpop.viewmodel.AnimeListViewModelFactory
@@ -27,35 +19,21 @@ class AnimeListActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: AnimeListAdapter
-    private lateinit var titleTextView: TextView
-    private lateinit var imageView: ImageView
-    private lateinit var episodesTextView: TextView
-    private lateinit var statusTextView: TextView
-    private lateinit var airedFromTextView: TextView
-    private lateinit var airedToTextView: TextView
     private lateinit var animeListViewModel: AnimeListViewModel
     private var remoteApi = RemoteApi
     private var animeRepository: AnimeRepository = AnimeRepository(remoteApi)
 
-    private var animeList: MutableList<AnimeModel> = mutableListOf()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anime_list)
-        Log.d("AnimeListActivity", "sono AnimeListActivity")
 
-//        titleTextView = findViewById(R.id.titleTextView)
-//        imageView = findViewById(R.id.animeImage)
-//        episodesTextView = findViewById(R.id.episodesTextView)
-//        statusTextView = findViewById(R.id.statusTextView)
-//        airedFromTextView = findViewById(R.id.airedFromTextView)
-//        airedToTextView = findViewById(R.id.airedToTextView)
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
         recyclerView = findViewById(R.id.recyclerView)
+        adapter = AnimeListAdapter(mutableListOf()) // Inizializza l'adattatore con una lista vuota
 
         recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = AnimeListAdapter()
         recyclerView.adapter = adapter
+
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.menu_animeList -> {
@@ -84,22 +62,11 @@ class AnimeListActivity : AppCompatActivity() {
             ViewModelProvider(this, AnimeListViewModelFactory(this.remoteApi))[AnimeListViewModel::class.java]
 
         animeListViewModel.animeList.observe(this, Observer { anime ->
-            animeList.addAll(anime)
-            Log.d("AnimeListActivity", animeList[0].title)
-
-            Picasso.get().load(animeList[8].images.jpg.image_url)
-                .placeholder(R.drawable.placeholder_image)
-                .into(imageView)
-
-            // Imposta il titolo dell'anime
-            titleTextView.text = animeList[8].title
-            episodesTextView.text = animeList[8].episodes.toString()
-            statusTextView.text = animeList[8].status
-            airedFromTextView.text = animeList[8].aired.from
-            airedToTextView.text = animeList[8].aired.to
+            adapter.setData(anime)
         })
 
         animeListViewModel.getTopAnime()
     }
 }
+
 
--- app/src/main/java/studio/demo/subarashi_jpop/adapters/AnimeListAdapter.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/adapters/AnimeListAdapter.kt
@@ -8,34 +8,42 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import studio.demo.subarashi_jpop.R
-import studio.demo.subarashi_jpop.remote.model.AnimeListResponse
 import studio.demo.subarashi_jpop.remote.model.AnimeModel
 
-class AnimeListAdapter(private val animeList: List<AnimeModel>):
-    RecyclerView.Adapter<AnimeListAdapter.AnimeViewHolder>(){
-    class AnimeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        val imageView: ImageView = itemView.findViewById(R.id.itemImageView)
+class AnimeListAdapter(private var animeList: List<AnimeModel>) : RecyclerView.Adapter<AnimeListAdapter.AnimeViewHolder>() {
+
+    class AnimeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val animeImage: ImageView = itemView.findViewById(R.id.itemImageView)
         val titleTextView: TextView = itemView.findViewById(R.id.itemTitleTextView)
         val episodesTextView: TextView = itemView.findViewById(R.id.itemEpisodesTextView)
         val statusTextView: TextView = itemView.findViewById(R.id.itemStatusTextView)
     }
 
+    fun setData(newAnimeList: List<AnimeModel>) {
+        animeList = newAnimeList
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_anime, parent, false)
-        return AnimeViewHolder(itemView)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_anime, parent, false)
+        return AnimeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: AnimeViewHolder, position: Int) {
-        val currentAnime = animeList[position]
+        val anime = animeList[position]
 
-        Picasso.get().load(currentAnime.images.jpg.image_url).placeholder(R.drawable.placeholder_image).into(holder.imageView)
+        // Popola gli elementi dell'UI con i dati dell'anime
+        Picasso.get().load(anime.images.jpg.image_url)
+            .placeholder(R.drawable.placeholder_image)
+            .into(holder.animeImage)
 
-        holder.titleTextView.text = currentAnime.title
-        holder.episodesTextView.text = "Episodes: ${currentAnime.episodes}"
-        holder.statusTextView.text = "Status: ${currentAnime.status}"
+        holder.titleTextView.text = anime.title
+        holder.episodesTextView.text = anime.episodes.toString()
+        holder.statusTextView.text = anime.status
     }
 
     override fun getItemCount(): Int {
         return animeList.size
     }
 }
+
