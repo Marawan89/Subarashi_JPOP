0e013f40b27a86214c453f3a9c9025ffd7b6b600
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-11-18 13:45:03 +0100

    added layout for favourite list and manga list, and added button

--- app/src/main/java/studio/demo/subarashi_jpop/activities/MangaListActivity.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/activities/MangaListActivity.kt
@@ -1,7 +1,12 @@
 package studio.demo.subarashi_jpop.activities
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import studio.demo.subarashi_jpop.R
 
 class MangaListActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manga_list)
+    }
 }
