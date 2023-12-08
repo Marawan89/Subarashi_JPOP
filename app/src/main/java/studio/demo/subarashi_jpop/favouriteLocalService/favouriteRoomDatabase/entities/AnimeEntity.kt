f6e7dbbc6e5ac4b013990163f2c2ecdc70adc3fe
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-12-08 14:42:09 +0100

    AnimeEntity implemented

--- app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/favouriteRoomDatabase/entities/AnimeEntity.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/favouriteRoomDatabase/entities/AnimeEntity.kt
@@ -1,4 +1,12 @@
 package studio.demo.subarashi_jpop.favouriteLocalService.favouriteRoomDatabase.entities
 
-class AnimeEntity {
-}
+import android.widget.ImageView
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favourite_anime")
+data class AnimeEntity (
+    @PrimaryKey val id: Int,
+    val title: String,
+    val imageUrl: ImageView
+)
