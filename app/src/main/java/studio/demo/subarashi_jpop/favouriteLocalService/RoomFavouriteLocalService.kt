e62cc54bc08f31e0cfa800a9c13def89582a992e
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-12-08 14:48:05 +0100

    RoomFavouriteLocalService implemented

--- app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/RoomFavouriteLocalService.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/RoomFavouriteLocalService.kt
@@ -1,4 +1,18 @@
 package studio.demo.subarashi_jpop.favouriteLocalService
 
-class RoomFavouriteLocalService {
+import studio.demo.subarashi_jpop.favouriteLocalService.favouriteRoomDatabase.dao.AnimeDao
+import studio.demo.subarashi_jpop.favouriteLocalService.favouriteRoomDatabase.entities.AnimeEntity
+
+class RoomFavouriteLocalService (private val animeDao: AnimeDao) : FavouriteLocalService {
+    override suspend fun insertAnime(anime: AnimeEntity){
+        animeDao.insertAnime(anime)
+    }
+
+    override suspend fun getFavouriteAnime(): List<AnimeEntity> {
+        return animeDao.getFavouriteAnime()
+    }
+
+    override suspend fun deleteAnime(anime: AnimeEntity){
+        animeDao.deleteAnime(anime)
+    }
 }
