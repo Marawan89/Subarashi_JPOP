498de4d556d0d0737834753c0c13d95642698306
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-12-08 14:37:41 +0100

    AnimeDao implemented

--- app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/favouriteRoomDatabase/dao/AnimeDao.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/favouriteRoomDatabase/dao/AnimeDao.kt
@@ -1,4 +1,20 @@
 package studio.demo.subarashi_jpop.favouriteLocalService.favouriteRoomDatabase.dao
 
-class AnimeDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import studio.demo.subarashi_jpop.favouriteLocalService.favouriteRoomDatabase.entities.AnimeEntity
+
+@Dao
+interface AnimeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAnime(anime: AnimeEntity)
+
+    @Query("SELECT * FROM favourite_anime")
+    suspend fun getFavouriteAnime(): List<AnimeEntity>
+
+    @Delete
+    suspend fun deleteAnime(anime: AnimeEntity)
 }
