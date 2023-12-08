937d36efab253ad60db530e835916fea53c36d5e
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-12-08 14:49:45 +0100

    FavouriteLocalService implemented

--- app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/FavouriteLocalService.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/favouriteLocalService/FavouriteLocalService.kt
@@ -1,4 +1,9 @@
 package studio.demo.subarashi_jpop.favouriteLocalService
 
+import studio.demo.subarashi_jpop.favouriteLocalService.favouriteRoomDatabase.entities.AnimeEntity
+
 interface FavouriteLocalService {
+    suspend fun insertAnime(anime: AnimeEntity)
+    suspend fun getFavouriteAnime(): List<AnimeEntity>
+    suspend fun deleteAnime(anime: AnimeEntity)
 }
