a93ee4efe349ce0daa116b9c6a6d86fc8cb780b0
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-12-11 23:18:19 +0100

    search query added in animeService

--- app/src/main/java/studio/demo/subarashi_jpop/remote/anime/AnimeService.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/remote/anime/AnimeService.kt
@@ -9,4 +9,8 @@ import studio.demo.subarashi_jpop.remote.anime.model.AnimeListResponse
 interface AnimeService {
     @GET("top/anime")
     suspend fun getTopAnime(@Query("page") page: Int = 1, @Query("per_page") perPage: Int = 10): AnimeListResponse
+
+    @GET("search/anime")
+    suspend fun searchAnime(@Query("q") query: String): AnimeListResponse
+
 }
