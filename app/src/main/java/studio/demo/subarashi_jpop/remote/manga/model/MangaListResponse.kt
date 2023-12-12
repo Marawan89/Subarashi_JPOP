8c128d0926bca623eaac7a3e09c6bcb0e5ebccfd
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2023-12-12 12:11:04 +0100

    Manga search working

--- app/src/main/java/studio/demo/subarashi_jpop/remote/manga/model/MangaListResponse.kt
+++ app/src/main/java/studio/demo/subarashi_jpop/remote/manga/model/MangaListResponse.kt
@@ -1,7 +1,5 @@
 package studio.demo.subarashi_jpop.remote.manga.model
 
-import studio.demo.subarashi_jpop.remote.anime.model.Pagination
-
 data class MangaListResponse(
     val pagination: Pagination,
     val data: List<MangaResponseModel>
